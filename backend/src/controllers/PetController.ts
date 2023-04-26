20736e96d8e9fe25907373aac954c5fac252ac9a
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-25 23:58:19 -0300

    Rota create pet

--- backend/src/controllers/PetController.ts
+++ backend/src/controllers/PetController.ts
@@ -3,6 +3,10 @@ import { Pet } from "../models/Pet";
 import { IUser } from "../interfaces/IUser";
 import { constants } from "fs/promises";
 
+// helpers
+import { getToken } from "../helpers/getToken";
+import { getUserByToken } from "../helpers/getUserByToken";
+
 // interface RegisterRequest extends Request {
 //   body: {
 //     name: string;
@@ -14,6 +18,46 @@ import { constants } from "fs/promises";
 
 export class PetController {
   static async create(req: Request, res: Response) {
-    res.json({ message: "Deu certo!" });
+    const { name, age, weight, color } = req.body;
+
+    const available = true;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    }
+    if (!age) {
+      res.status(422).json({ message: "A idade é obrigatória!" });
+      return;
+    }
+    if (!weight) {
+      res.status(422).json({ message: "O peso é obrigatório!" });
+      return;
+    }
+    if (!color) {
+      res.status(422).json({ message: "A cor é obrigatória!" });
+      return;
+    }
+
+    // get pet owner
+    const token = getToken(req);
+    const user = getUserByToken(token as string);
+
+    // create a pet
+    const pet = new Pet({
+      name: name,
+      age: age,
+      weight: weight,
+      color: color,
+      available: available,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    });
   }
 }
--- backend/src/routes/PetRoutes.ts
+++ backend/src/routes/PetRoutes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 import { PetController } from "../controllers/PetController";
 
+import { verifyToken } from "../middlewares/verifyToken";
+
 const router = Router();
 
-router.post("/create", PetController.create);
+router.post("/create", verifyToken, PetController.create);
 
 export default router;
