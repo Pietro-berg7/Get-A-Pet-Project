dfe125d95e6dc69d43f14f2a907540087e9514f8
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-28 01:33:24 -0300

    Criando página e formulário de registro

---
+++ frontend/src/components/form/Input.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./Input.css";
+
+interface InputProps {
+  type: string;
+  text: string;
+  name: string;
+  placeholder: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  multiple?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  type,
+  text,
+  name,
+  placeholder,
+  handleChange,
+  value,
+  multiple,
+}) => {
+  return (
+    <div>
+      <label htmlFor={name}>{text}:</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={handleChange}
+        value={value}
+        {...(multiple ? { multiple } : "")}
+      />
+    </div>
+  );
+};
+
+export default Input;
--- frontend/src/components/pages/Auth/Register.tsx
+++ frontend/src/components/pages/Auth/Register.tsx
@@ -1,9 +1,52 @@
 import React from "react";
+import Input from "../../form/Input";
 
 const Register: React.FC = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <section>
-      <h1>Register</h1>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleChange={handleChange}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleChange={handleChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          handleChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
     </section>
   );
 };
