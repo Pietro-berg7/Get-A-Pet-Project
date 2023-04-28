83bfead49c5653fb682130a757ebf4e4d0b9678d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-28 00:50:12 -0300

    Estruturando navbar e footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>
+        <span className="bold">Get A Pet</span> &copy; 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/layout/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Logo from "../../assets/img/logo.png";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div>
+        <img src={Logo} alt="Get A Pet" />
+        <h2>Get A Pet</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Adotar</Link>
+        </li>
+        <li>
+          <Link to="/login">Entrar</Link>
+        </li>
+        <li>
+          <Link to="/register">Cadastrar</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
