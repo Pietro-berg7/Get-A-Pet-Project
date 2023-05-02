373a1fac7135b2db4e72e3962f810a1d96aafa79
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-01 23:28:05 -0300

    Formulário de login

--- frontend/src/components/pages/Auth/Login.tsx
+++ frontend/src/components/pages/Auth/Login.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useContext, SyntheticEvent } from "react";
+import Input from "../../form/Input";
+
+import "../../form/Form.css";
+
+import { Context } from "../../../context/UserContext";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
+  const handleChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    console.log(e);
+    // setUser({ ...user, [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <section>
+    <section className="form_container">
       <h1>Login</h1>
+      <form>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          handleChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleChange={handleChange}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem conta? <Link to="/register">Clique aqui!</Link>
+      </p>
     </section>
   );
 };
