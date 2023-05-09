d42f369285cd486cd872ff6364d16a41321ee194
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-09 18:27:42 -0300

    Finalizando página de adoções

--- frontend/src/components/pages/Pet/MyAdoptions.tsx
+++ frontend/src/components/pages/Pet/MyAdoptions.tsx
@@ -3,6 +3,7 @@ import api from "../../../utils/api";
 import RoundedImage from "../../layout/RoundedImage";
 
 import "./Dashboard.css";
+import { IPet } from "../../../interfaces/IPet";
 
 const MyAdoptions: React.FC = () => {
   const [pets, setPets] = useState([]);
@@ -20,7 +21,44 @@ const MyAdoptions: React.FC = () => {
       });
   }, [token]);
 
-  return <div />;
+  const apiURL = "http://localhost:5000";
+
+  return (
+    <section>
+      <div className="petlist_header">
+        <h1>Minhas adoções</h1>
+      </div>
+      <div className="petlist_container">
+        {pets.length > 0 &&
+          pets.map((pet: IPet) => (
+            <div className="petlist_row" key={pet._id}>
+              <RoundedImage
+                src={`${apiURL}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className="contacts">
+                <p>
+                  <span className="bold">Ligue para:</span> {pet.user.phone}
+                </p>
+                <p>
+                  <span className="bold">Fale com:</span> {pet.user.name}
+                </p>
+              </div>
+              <div className="actions">
+                {pet.available ? (
+                  <p>Adoção em processo</p>
+                ) : (
+                  <p>Parabéns por concluir a adoção</p>
+                )}
+              </div>
+            </div>
+          ))}
+        {pets.length === 0 && <p>Ainda não há adoções de Pets.</p>}
+      </div>
+    </section>
+  );
 };
 
 export default MyAdoptions;
--- frontend/src/interfaces/IPet.ts
+++ frontend/src/interfaces/IPet.ts
@@ -1,3 +1,5 @@
+import { IUser } from "./IUser";
+
 export interface IPet {
   _id: string;
   name: string;
@@ -6,5 +8,6 @@ export interface IPet {
   color: string;
   images: FileList;
   available: boolean;
+  user: IUser;
   adopter?: object;
 }
