9b90a3dccb5a70f88307158ed7f2087aa0b4cbf4
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-09 16:50:50 -0300

    Página de detalhes do pet

---
+++ frontend/src/components/pages/Pet/PetDetails.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+import { useParams, Link } from "react-router-dom";
+
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
+import "./PetDetails.css";
+
+const PetDetails: React.FC = () => {
+  return (
+
+  );
+};
+
+export default PetDetails;
