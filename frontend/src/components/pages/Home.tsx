7a0a662ab440f3f936f11d635c57a3ecb273a7cc
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-05-09 15:25:36 -0300

    Resgatando os pets na home

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../utils/api";
+
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    api.get("/pets").then((response) => {
+      setPets(response.data.pets);
+    });
+  }, []);
+
   return (
     <section>
       <h1>Home</h1>
